a8d55499e9c6dcc692bb99c78b2f3b691627305c
unknown <Hiromi.Kaede@gmail.com>
2016-02-13 11:20:36 +0800

    修正几处主程序文本位置错误

--- tools/text_update.py
+++ tools/text_update.py
@@ -7,9 +7,27 @@ def main():
     for t in text:
         original = t['original']
         if original:
-            original = original.encode('shiftjis') + b'\x00'
-            newrva = dump.find(original)
-            t['rva'] = '%08X' % newrva
+            original = original.encode('shiftjis')
+            original += (4 - (len(original) % 4)) * b'\x00'
+
+            start = 0
+            while start < len(dump):
+                newrva = dump.find(original, start)
+                if newrva == -1:
+                    start = len(dump)
+                    continue
+
+                if newrva % 4 != 0:
+                    start = newrva + len(original)
+                    if start == 7: ibp()
+                    continue
+
+                t['rva'] = '%08X' % newrva
+                break
+
+            else:
+                print('missing %s' % original)
+                # ibp()
 
     open('ed6_fc_text_update.json', 'wb').write(json.dumps(text, ensure_ascii = False, indent = '  ').encode('utf_8_sig'))
 
--- tools/update.bat
+++ tools/update.bat
@@ -1,6 +1,7 @@
 @echo off
 cd/d "%~dp0"
 
+move /y ed6_fc_text_update.json ed6_fc_text.json
 call gentextbin.py
 move /y ed6fc.text "D:\Game\Steam\steamapps\common\Trails in the Sky FC"
 start "" "D:\Game\Steam\steamapps\common\Trails in the Sky FC\ed6_win_crack.exe"
