5b14b4e46390180cdf92129da332060bbfbb28ff
Ouroboros <Hiromi.Kaede@gmail.com>
2017-06-26 01:29:01 +0800

    补全小地图地标翻译

--- tools/update.bat
+++ tools/update.bat
@@ -3,5 +3,5 @@ cd/d "%~dp0"
 
 move /y ed6_fc_text_update.json ed6_fc_text.json
 call gentextbin.py
-move /y ed6fc.text "D:\Game\Steam\steamapps\common\Trails in the Sky FC"
-start "" "D:\Game\Steam\steamapps\common\Trails in the Sky FC\ed6_win.exe"
+move /y ed6fc.text "E:\Game\Steam\steamapps\common\Trails in the Sky FC"
+start "" "E:\Game\Steam\steamapps\common\Trails in the Sky FC\ed6_win.exe"
